3e7870650373435c77669611beaf0f63a75ae400
79375 <79375@DESKTOP-4JDPJVR>
2024-05-04 19:27:00 +0300

    Добавьте файлы проекта.

---
+++ Typograph.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace Typograph
+{
+    partial class Typograph
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Typograph));
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.оттипографитьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.справкаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.zOVSVOToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox1.Location = new System.Drawing.Point(0, 30);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(800, 420);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.оттипографитьToolStripMenuItem,
+            this.zOVSVOToolStripMenuItem,
+            this.справкаToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // оттипографитьToolStripMenuItem
+            // 
+            this.оттипографитьToolStripMenuItem.Name = "оттипографитьToolStripMenuItem";
+            this.оттипографитьToolStripMenuItem.Size = new System.Drawing.Size(104, 20);
+            this.оттипографитьToolStripMenuItem.Text = "Оттипографить";
+            this.оттипографитьToolStripMenuItem.Click += new System.EventHandler(this.оттипографитьToolStripMenuItem_Click);
+            // 
+            // справкаToolStripMenuItem
+            // 
+            this.справкаToolStripMenuItem.Name = "справкаToolStripMenuItem";
+            this.справкаToolStripMenuItem.Size = new System.Drawing.Size(65, 20);
+            this.справкаToolStripMenuItem.Text = "Справка";
+            this.справкаToolStripMenuItem.Click += new System.EventHandler(this.справкаToolStripMenuItem_Click);
+            // 
+            // zOVSVOToolStripMenuItem
+            // 
+            this.zOVSVOToolStripMenuItem.AutoToolTip = true;
+            this.zOVSVOToolStripMenuItem.Name = "zOVSVOToolStripMenuItem";
+            this.zOVSVOToolStripMenuItem.Size = new System.Drawing.Size(67, 20);
+            this.zOVSVOToolStripMenuItem.Text = "ZOV SVO";
+            this.zOVSVOToolStripMenuItem.ToolTipText = "заменяет каждую букву на сво";
+            this.zOVSVOToolStripMenuItem.Click += new System.EventHandler(this.zOVSVOToolStripMenuItem_Click);
+            // 
+            // Typograph
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Typograph";
+            this.Text = "Typograph";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem оттипографитьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem справкаToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem zOVSVOToolStripMenuItem;
+    }
+}
+
---
+++ Typograph.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Typograph
+{
+    public partial class Typograph : Form
+    {
+        public Typograph()
+        {
+            InitializeComponent();
+        }
+
+        private void оттипографитьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(richTextBox1.Text))
+            {
+                MessageBox.Show("Вы ничего не ввели!");
+                return;
+            }
+            var regex = new Regex("\\s+");
+            richTextBox1.Text = regex.Replace(richTextBox1.Text, " ");
+
+            regex = new Regex("(\\+-)|(-\\+)|(\\(\\+,-\\))");
+            richTextBox1.Text = regex.Replace(richTextBox1.Text, "±");
+
+            regex = new Regex("\\s+(?=[.,?!:;])"); 
+
+            richTextBox1.Text = regex.Replace(richTextBox1.Text, "");
+
+            regex = new Regex("(?<=[,.!?;:])(?=\\w)");
+
+            richTextBox1.Text = regex.Replace(richTextBox1.Text, " ");
+
+            regex = new Regex("\\s*—\\s*");
+            richTextBox1.Text = regex.Replace(richTextBox1.Text, "\u00a0— ");
+
+            regex = new Regex("\\s*-\\s*");
+            richTextBox1.Text = regex.Replace(richTextBox1.Text, "-");
+
+            regex = new Regex("\\.{2,}");
+            richTextBox1.Text = regex.Replace(richTextBox1.Text, "...");
+
+            richTextBox1.Text = richTextBox1.Text.Replace("ё", "е");
+
+            richTextBox1.Text = richTextBox1.Text.Trim();
+
+
+
+
+        }
+
+        private void справкаToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Это типограф? Да, это типограф! Версия секретный ЗОВ.\nПравило 1) Не больше 1 пробела подряд.\r\nПравило 2) Все знаки препинания пишутся слева слитно со словом, а справа отбиваются\r\nпробелом.\r\nПравило 3) Дефис пробелами не отбивается.\r\nПравило 4) С обоих сторон от тире ставится пробел.\r\nПравило 5) Многоточие заменяется на символ троеточия.\r\nПравило 6) Символ «плюс-минус» задаётся так: ±\r\nПравило 7) Каждая буква ё меняется на е.\r\nСекретное правило ZOV : При нажатии на секретную кнопку каждая буква меняется на сво.");
+        }
+
+        private void zOVSVOToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(richTextBox1.Text))
+            {
+                MessageBox.Show("Вы ничего не ввели!");
+                return;
+            }
+
+            var regex = new Regex("\\w", RegexOptions.IgnoreCase);
+
+            richTextBox1.Text = regex.Replace(richTextBox1.Text, "сво ");
+            
+        }
+    }
+}
